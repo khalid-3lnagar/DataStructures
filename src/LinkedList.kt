54d908fe4785a1340a67a036bbf69bb8f94073b4
Khalid Elnagar <khalid.3lnagar@gmail.com>
2019-06-25 11:34:04 +0200

    Add: contains & remove functions

--- src/LinkedList.kt
+++ src/LinkedList.kt
@@ -1,7 +1,51 @@
-class Node(var value: Int?, var next: Node? = null)
+const val STRING_LINE = "\n\t************************************************\n"
+
+fun main() {
+    val linkedList = LinkedList()
+
+    with(linkedList) {
+        //Add some data
+        addFirst(Node(5))
+        addFirst(Node(0))
+        addFirst(Node(7))
+
+        //print that data
+        printList(getFirst())
+        println(size())
+
+        //try contains method
+        contains(0).also(::println)
+        println(linkedList.size())
+
+        println(STRING_LINE)
+        removeByValue(0)
+        println(STRING_LINE)
+
+        contains(0).also(::println)
+
+        printList(getFirst())
+        println(linkedList.size())
+    }
+
+
+}
+
+fun printList(node: Node?) {
+    if (node == null) {
+        println()
+        return
+    }
+    with(node) {
+        println("value $value\nnext value: ${next?.value}\nprevious value: ${previous?.value}")
+    }
+    println(STRING_LINE)
+    printList(node.next)
+}
+
 class LinkedList(
     private var head: Node? = null,
     private var tail: Node? = null,
+    private var current: Node? = null,
     private var count: Int = 0
 ) {
 
@@ -12,8 +56,11 @@ class LinkedList(
     fun addFirst(newNode: Node) {
         if (count == 0)
             tail = newNode
-        else
+        else {
+            head?.previous = newNode
             newNode.next = head
+        }
+
 
         head = newNode
         count++
@@ -23,10 +70,12 @@ class LinkedList(
     fun getFirst() = head
 
     fun addLast(newNode: Node) {
-        if (count > 0)
-            tail?.next = newNode
-        else
+        if (count == 0)
             head = newNode
+        else {
+            tail?.next = newNode
+            newNode.previous = tail
+        }
 
         tail = newNode
         count++
@@ -35,63 +84,94 @@ class LinkedList(
     fun getLast() = tail
 
     fun removeFirst() {
-        if (count < 2)
-            initialState()
-        else
-            head = head!!.next
-
-        count--
+        when (count) {
+            0 -> return
+            1 -> clear()
+            else -> {
+                head?.next?.previous = null
+                head = head?.next
+                count--
+            }
+        }
     }
 
     fun removeLast() {
-        if (count < 2)
-            initialState()
-        else
-            removeLastNode(head!!)
+        when (count) {
+            0 -> return
+            1 -> clear()
+            else -> {
+                removeLastNode(head!!)
+                count--
+            }
+        }
 
-        count--
     }
 
     private fun removeLastNode(node: Node) {
-
         if (node.next == tail) {
             node.next = null
             tail = node
 
-            return
-        }
-        removeLastNode(node.next!!)
-
+        } else removeLastNode(node.next!!)
     }
 
-    private fun initialState() {
+    fun removeByValue(value: Int) {
+
+        if (contains(value)) {
+
+            when (value) {
+                head?.value -> head?.remove()
+
+                tail?.value -> tail?.remove()
+
+                else -> searchAndRemove(value)
+            }
+            count--
+
+        }
+    }
+
+    private fun searchAndRemove(value: Int) {
+        current = head
+        recursiveSearchByValue(value)?.remove()
+        current = null
+    }
+
+    private fun clear() {
         head = null
         tail = null
+        count = 0
     }
+
+    fun contains(value: Int): Boolean = if (head?.value == value || tail?.value == value) true else search(value)
+
+    private fun search(value: Int): Boolean {
+        current = head?.next
+        val isHere = recursiveSearchByValue(value) != null
+        current = null
+        return isHere
+    }
+
+    private fun recursiveSearchByValue(value: Int): Node? {
+        if (current == tail) return null
+
+        return if (current?.value == value)
+            current
+        else {
+            current = current?.next
+            recursiveSearchByValue(value)
+        }
+    }
+
+    fun size() = count
 
 }
 
-fun main() {
-    val linkedList = LinkedList(Node(5))
+class Node(var value: Int?, var next: Node? = null, var previous: Node? = null)
 
-
-    linkedList.addFirst(Node(3))
-
-
-    linkedList.addLast(Node(7))
-
-    for (i: Int in 0..4) {
-        printList(linkedList.getFirst())
-        linkedList.removeLast()
-
-    }
+fun Node.remove() {
+    next?.previous = previous
+    previous?.next = next
 }
 
-fun printList(node: Node?) {
-    if (node == null) {
-        println()
-        return
-    }
-    print(node.value)
-    printList(node.next)
-}
+
